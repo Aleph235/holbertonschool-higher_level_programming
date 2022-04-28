7184255c26a7370ff53593e1615845869dde11aa
Aleph235 <akram239911@gmail.com>
2022-04-28 12:46:19 +0200

    well well wll

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+n = len(sys.argv)
+if n < 2:
+    print("0 arguments.")
+elif n > 2:
+    print(f"{n - 1} arguments:")
+else:
+    print("1 argument:")
+for i in range(1, n):
+    if n == 2:
+        print(f"{i}: {sys.argv[i]}")
+    else:
+        print(f"{i}: {sys.argv[i]}")
