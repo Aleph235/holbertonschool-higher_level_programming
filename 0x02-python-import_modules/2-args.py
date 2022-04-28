7e8c9a3f76d5a372cb3423637c3671ef7a2e0c03
Aleph235 <akram239911@gmail.com>
2022-04-28 14:32:21 +0200

    imported must not execute when imported

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 import sys
-n = len(sys.argv)
-if n < 2:
-    print("0 arguments.")
-elif n > 2:
-    print(f"{n - 1} arguments:")
-else:
-    print("1 argument:")
-for i in range(1, n):
-    if n == 2:
-        print(f"{i}: {sys.argv[i]}")
+if __name__ == "__main__":
+    n = len(sys.argv)
+    if n < 2:
+        print("0 arguments.")
+    elif n > 2:
+        print(f"{n - 1} arguments:")
     else:
-        print(f"{i}: {sys.argv[i]}")
+        print("1 argument:")
+    for i in range(1, n):
+        if n == 2:
+            print(f"{i}: {sys.argv[i]}")
+        else:
+            print(f"{i}: {sys.argv[i]}")
