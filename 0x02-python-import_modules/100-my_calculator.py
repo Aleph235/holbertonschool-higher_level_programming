2a7cfa43b05aba80d98e26b7df8cf5e1ae1b7c4c
Aleph235 <akram239911@gmail.com>
2022-04-29 17:36:58 +0200

    pep8

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,8 +3,11 @@ import sys
 from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
     n = len(sys.argv)
-    operator = ('+', '-', '*', '/')
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    operators = ('+','-','*','/')
     def apply_operation(a, b):
+        """applies the intended operation to two integers"""
         if sys.argv[2] == '+':
             return add(a, b)
         elif sys.argv[2] == '-':
@@ -19,10 +22,8 @@ if __name__ == "__main__":
     if n != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    elif sys.argv[2] not in operator:
+    elif sys.argv[2] not in operators:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     else:
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
         print(f'{a} {sys.argv[2]} {b} = {apply_operation(a, b)}')
