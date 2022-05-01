fde67364885d8758a3036ed286806b6e01322887
Akram Jerbi <98221645+Aleph235@users.noreply.github.com>
2022-05-01 12:18:30 +0200

    Delete 0x01-python-if-else-loops_functions directory

--- 0x01-python-if-else-loops_functions/3-print_alphabt.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-for item in range(97,123):
-    if item == 101 or item == 113:
-        continue
-    else:
-        print(chr(item), end="")
--- 0x01-python-if-else-loops_functions/5-print_comb2.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-for item in range(100):
-    if item == 99:
-        print(item)
-    else:
-        print("{:02}".format(item), end=", ")
