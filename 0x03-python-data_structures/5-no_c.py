b5ab22aa71b9568df31d51baabcfb8476b7d381e
Aleph235 <akram239911@gmail.com>
2022-05-02 22:14:53 +0200

    nerve racking

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    global new_str
     for i in range(len(my_string)):
-        if my_string[0] == "C":
-            new_str = my_string[1:]
-        if my_string[i] == "c" and i > 0:
+        if my_string[0] == "C" and my_string[i] == "c":
+            new_str = my_string[1:i] + my_string[i+1:]
+        elif my_string[i] in ("c", "C"):
             new_str = my_string[:i] + my_string[i+1:]
-            if my_string[i] == "C" and i > 0:
-                new_str = new_str[:i] + new_str[i+1:]
     return new_str
-
-print(no_c("Chicago"))
-print(no_c("Best School"))
