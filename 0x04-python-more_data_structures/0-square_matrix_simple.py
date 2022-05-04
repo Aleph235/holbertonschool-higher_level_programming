9e6922997c103b3b7a780de0895468f9abd30e62
Aleph235 <akram239911@gmail.com>
2022-05-04 14:04:32 +0200

    codestyle correction

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,10 +2,11 @@
 def square_matrix_simple(matrix=[]):
     squared_matrix = []
     new_matrix = []
-    for inner_list in matrix:
-        for element in inner_list:
-            squared_matrix.append([element**2 for element in inner_list])
-    for element in squared_matrix:
-        if element not in new_matrix:
-            new_matrix.append(element)
-    return new_matrix
+    if matrix:
+        for inner_list in matrix:
+            for element in inner_list:
+                squared_matrix.append([element**2 for element in inner_list])
+        for element in squared_matrix:
+            if element not in new_matrix:
+                new_matrix.append(element)
+        return new_matrix
