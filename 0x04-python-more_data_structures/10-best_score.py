60fdb77eee5eca41929e9213c0e6b8cca4166cf1
Aleph235 <akram239911@gmail.com>
2022-05-04 17:34:34 +0200

    sort values and find key

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,5 +3,7 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     else:
-        value_sorted_dictionary = sorted(a_dictionary.values())
-        return value_sorted_dictionary[len(value_sorted_dictionary)-1]
+        value_sorted_list = sorted(a_dictionary.values())
+        for key, value in a_dictionary.items():
+            if value_sorted_list[-1] == value:
+                return key
