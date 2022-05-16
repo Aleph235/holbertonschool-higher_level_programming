e3473e4db6640d0e3ccf736e17cba0fae30cbcf3
Aleph235 <akram239911@gmail.com>
2022-05-16 14:29:35 +0200

    I finally found how to print new line

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,9 +2,10 @@
 def safe_print_list(my_list=[], x=0):
     try:
         elements_printed_count = 0
-        for i in range(0, x):
-            print(f'{my_list[i]}', end="")
+        for i in range(x):
+            print(my_list[i], end='')
             elements_printed_count += 1
     except IndexError:
         pass
+    print()
     return elements_printed_count
