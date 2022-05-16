b7e331baf89c6d53653093e953fe1e3bf83c8d27
Aleph235 <akram239911@gmail.com>
2022-05-16 19:51:24 +0200

    add keyword exception

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,7 +4,7 @@ import sys
 
 def safe_print_integer_err(value):
     message_1 = "Exception: Unknown format code 'd' for object of type 'str'"
-    message_2 = "unsupported format string passed to tuple.__format__"
+    message_2 = "Exception: unsupported format string passed to tuple.__format__"
     try:
         print("{:d}".format(value))
         return True
@@ -14,3 +14,23 @@ def safe_print_integer_err(value):
     except (TypeError):
         print(message_2, file=sys.stderr)
         return False
+
+value = 89
+has_been_print = safe_print_integer_err(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = -89
+has_been_print = safe_print_integer_err(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = "School"
+has_been_print = safe_print_integer_err(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = 1,4 
+has_been_print = safe_print_integer_err(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
