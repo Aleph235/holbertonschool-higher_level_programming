7c886e4aa1d2d11efafceadd2523fdc06df4253b
Aleph235 <akram239911@gmail.com>
2022-05-16 14:14:43 +0200

    put your blocks in the right place

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    elements_printed_count = 0
-
-    for i in range(0, x):
-        try:
+    try:
+        elements_printed_count = 0
+        for i in range(0, x):
             print(f'{my_list[i]}', end="")
             elements_printed_count += 1
-        except IndexError:
-            break
+    except IndexError:
+        pass
     return elements_printed_count
