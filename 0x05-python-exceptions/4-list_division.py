bfa9eb03cb099eb545175c8a794dad52a6c37678
Aleph235 <akram239911@gmail.com>
2022-05-16 18:49:26 +0200

    intialising memory block

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     try:
-        result = []
+        result = [0]*list_length
         for i in range(list_length):
             try:
                 result.append(my_list_1[i]/my_list_2[i])
@@ -15,6 +15,5 @@ def list_division(my_list_1, my_list_2, list_length):
                 continue
     except IndexError:
         print("out of range")
-        result.append(0)
     finally:
         return result
