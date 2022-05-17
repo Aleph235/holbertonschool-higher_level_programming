72cae8930815b155009c3cc2b9ee1b4b34bdc09f
Aleph235 <akram239911@gmail.com>
2022-05-17 17:42:40 +0200

    please do not disturb

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""square class with private attribute size"""
+
+
+class Square:
+    """square class with private attribute size"""
+    def __init__(self, size=0):
+        """Class method to initialize the square object"""
+        self.__size = size
