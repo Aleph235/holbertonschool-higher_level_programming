bebfa0d2dba056b8b6df2f793c85549a6e8f09f7
Aleph235 <akram239911@gmail.com>
2022-05-31 00:43:34 +0200

    stil lacking isint checks

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""class rectangle"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """class rectangle"""
+    def __init__(self, width, height):
+        """initialising rectangle"""
+        self.__width = width
+        self.__height = height
+        super().__init__()
+    def area(self.width, self.height):
+        """calculates the area of a Rectangle"""
+        return self.width * self.height
+    def __str__(self):
+        """print the retangle"""
+        return f"[Rectangle] {width}/{height}"
