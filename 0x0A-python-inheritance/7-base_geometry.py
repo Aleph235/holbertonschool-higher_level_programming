2530b7390526a3c9ddbeb3f5ea1d25d9684f8389
Aleph235 <akram239911@gmail.com>
2022-05-30 22:48:27 +0200

    adding function doc string

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -8,6 +8,7 @@ class BaseGeometry:
         """raise exception for non implemeted method"""
         raise Exception("area() is not implemented")
     def integer_validator(self, name, value):
+        """validate value if it is an int and strictly positive"""
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
