4a9dce464c9fd3ca0a49e8f7235d929401099aec
Aleph235 <akram239911@gmail.com>
2022-05-30 17:50:20 +0200

    same class of inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+
+def is_kind_of_class(obj, a_class):
+    """returns True if the object is instance amd subclass of a class"""
+    return isinstance(obj, a_class) and issubclass(a_class, a_class)
