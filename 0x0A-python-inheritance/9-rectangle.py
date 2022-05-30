8a9a8b467877ab00617aba2d1c509fcbf2bb1c6b
Aleph235 <akram239911@gmail.com>
2022-05-31 00:44:32 +0200

    fixing pycodestyle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -10,9 +10,11 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.__height = height
         super().__init__()
+
     def area(self.width, self.height):
         """calculates the area of a Rectangle"""
         return self.width * self.height
+
     def __str__(self):
         """print the retangle"""
         return f"[Rectangle] {width}/{height}"
