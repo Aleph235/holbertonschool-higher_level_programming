00bfe940e33f0831e02cfa8a57d575615addf88b
Aleph235 <akram239911@gmail.com>
2022-05-30 20:19:03 +0200

    built in sort

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,14 +6,4 @@ class MyList(list):
     """ Class MyList that inherits list and can sort ints"""
     def print_sorted(self):
         """print sorted list in ascending order"""
-        sorted_l = self[:]
-        for i in range(1, len(sorted_l)):
-            a = sorted_l[i]
-            j = i - 1
-
-            while j >= 0 and a < sorted_l[j]:
-                sorted_l[j + 1] = sorted_l[j]
-                j -= 1
-
-            sorted_l[j + 1] = a
-        return print(sorted_l)
+        return print(sorted(self))
