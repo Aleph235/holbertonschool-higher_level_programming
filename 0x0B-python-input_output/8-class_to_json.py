771f5a8599516d24279a94fbac5341d35aa60458
Aleph235 <akram239911@gmail.com>
2022-05-31 19:12:38 +0200

    fix

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """returns json serialization of an object"""
-import json
 
 
 def class_to_json(obj):
     """returns json serialization of an object"""
-    return json.dumps(obj.__dict__)
+    return obj.__dict__
