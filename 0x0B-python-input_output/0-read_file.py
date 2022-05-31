e41bb0b3c7b23de9bd8422502ef3f29ef6df9f72
Aleph235 <akram239911@gmail.com>
2022-05-31 18:41:54 +0200

    fixing pycode

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=''):
     """function that reads a text file (UTF8) and prints it to stdout"""
     with open(filename, mode='r',  encoding='utf-8') as my_file:
         for line in my_file:
-            print(line,end='')
+            print(line, end='')
