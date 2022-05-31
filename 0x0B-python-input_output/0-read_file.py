bd928842a729d2ead2c096747e8689eb6d297dea
Aleph235 <akram239911@gmail.com>
2022-05-31 18:40:25 +0200

    read line by line

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,5 @@
 def read_file(filename=''):
     """function that reads a text file (UTF8) and prints it to stdout"""
     with open(filename, mode='r',  encoding='utf-8') as my_file:
-        print(my_file.read())
+        for line in my_file:
+            print(line,end='')
