a5a580f4666d31818a86c49b8b3941ad62c570ff
Aleph235 <akram239911@gmail.com>
2022-05-31 19:15:02 +0200

    fix style

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -11,7 +11,6 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-
     def to_json(self):
         """retrieves dict repr of student instance"""
         return self.__dict__
