dbad4dc4fb7ca9fc1e3c99dcf802384956f6db44
Aleph235 <akram239911@gmail.com>
2022-05-31 19:13:55 +0200

    task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import json
+"""class student"""
+
+
+class Student:
+    """class student"""
+    def __init__(self, first_name, last_name, age):
+        """instantiate the class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+
+    def to_json(self):
+        """retrieves dict repr of student instance"""
+        return self.__dict__
