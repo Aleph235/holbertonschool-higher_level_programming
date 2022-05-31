c2bdc59a7d954663875f18a6a1aca16fd439ee0b
Aleph235 <akram239911@gmail.com>
2022-05-31 18:45:19 +0200

    adjusting docstring placement

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import json
 """function that creates an
 object from a JSON file"""
+import json
 
 
 def load_from_json_file(filename):
     """function that creates an
     object from a JSON file"""
-    with open(filename, encoding='utf-8') as my_file:
+    with open(filename, mode='w', encoding='utf-8') as my_file:
         return json.load(my_file)
