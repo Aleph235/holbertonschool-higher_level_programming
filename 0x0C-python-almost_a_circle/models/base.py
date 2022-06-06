99b783192f7070cb7041c94d06da0cb5eebeb570
Aleph235 <akram239911@gmail.com>
2022-06-06 16:31:16 +0200

    fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,8 +8,8 @@ class Base:
 
     def __init__(self, id=None):
         """initialiase base class"""
-        if id is None:
+        if id is not None:
+            self.id = id
+        else:
             self.id = Base.__nb_objects
             Base.__nb_objects += 1
-        else:
-            self.id = id
