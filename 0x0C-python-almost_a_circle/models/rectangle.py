42ca7fecc2e6413b3408540019597b28a67f63bd
Aleph235 <akram239911@gmail.com>
2022-06-06 17:30:45 +0200

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -88,7 +88,7 @@ class Rectangle(Base):
     def __str__(self):
         """print class rectangle"""
         return f"[Rectangle] ({self.id})"/
-        " {self.x}/{self.y} - {self.width}/{self.height}"
+        "{self.x}/{self.y} - {self.width}/{self.height}"
 
     def update(self, *args, **kwargs):
         """update rectangle instance"""
