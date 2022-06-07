4fd7b8d830589b4b3b22c3e4701206ca5cb9d0d3
Aleph235 <akram239911@gmail.com>
2022-06-07 11:09:57 +0200

    fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -78,8 +78,8 @@ class Rectangle(Base):
 
     def __str__(self):
         """print class rectangle"""
-        return f"[Rectangle] ({self.id})"/
-        "{self.x}/{self.y} - {self.width}/{self.height}"
+        return f"[Rectangle] ({self.id})\
+ {self.x}/{self.y} - {self.width}/{self.height}"
 
     def update(self, *args, **kwargs):
         """update rectangle instance"""
