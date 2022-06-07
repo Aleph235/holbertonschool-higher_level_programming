26df9d5f2fe6d149f1fc1f1936b00e54551dd585
Aleph235 <akram239911@gmail.com>
2022-06-07 19:03:26 +0200

    adding to json method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Define Base class"""
+import json
 
 
 class Base:
@@ -7,8 +8,16 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """initialise base attributes"""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """returns the json representation of list_dictionaries"""
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
