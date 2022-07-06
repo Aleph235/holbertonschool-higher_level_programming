98de35e16c25dcff56815b035972114072fe4a84
Aleph235 <akram239911@gmail.com>
2022-07-06 19:07:38 +0200

    fix

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 --lists all shows contained in the database hbtn_0d_tvshows.`
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows AS s LEFT JOIN tv_show_genres
+FROM tv_shows AS s LEFT JOIN tv_show_genres AS g
 ON s.id = g.show_id
 ORDER BY s.title ASC, g.genre_id ASC;
