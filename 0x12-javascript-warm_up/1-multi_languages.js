5d6c28591b7c3b861746c15228fb5f0555048f4b
Aleph235 <akram239911@gmail.com>
2022-08-01 17:41:29 +0200

    fix typo

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const name = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const name = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < name.length; i++) {
   console.log(name[i]);
