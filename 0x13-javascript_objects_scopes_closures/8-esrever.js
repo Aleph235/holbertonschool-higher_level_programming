53a20a0887c61a6995e034b03d6998d93713a410
Aleph235 <akram239911@gmail.com>
2022-08-02 21:06:39 +0200

    dont forget ur user bin node

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,6 @@
+#!/usr/bin/node
 exports.esrever = function (list) {
-  for (i = 0; i < list.length; i++) {
+  for (let i = 0; i < list.length; i++) {
     const tmp = list.splice(i, 1)[0];
     list.unshift(tmp);
   }
