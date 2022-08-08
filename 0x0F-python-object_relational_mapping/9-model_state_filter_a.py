8bf2635b7f8b6d92b4de5eb55f6425e916602070
Aleph235 <akram239911@gmail.com>
2022-08-08 12:03:18 +0200

    fix

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -3,19 +3,22 @@
 a from the database hbtn_0e_6_usa
 """
 import sys
-from model_state import Base, State
-
-from sqlalchemy import create_engine, Session
+from model_state import State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    path = ('mysql+mysqldb://{}:{}@localhost/{}'
+              .format(sys.argv[1], sys.argv[2],
+                      sys.argv[3]))
 
-    session = Session(engine)
+    engine = create_engine(path, pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
     for state in session.query(State)\
-                        .filter(State.like('%a%'))\
-                        .order_by(State.id):
-        print("{}: {}".format(state.id, state.name))
-    session.close()
+                        .filter(State.name.contains('a%')):
+                        if state is None:
+                            print("Nothing")
+                        else:
+                            print("{}: {}".format(state.id, state.name))
