cb894bd54ff220664b86b4eea898517ef45df1bc
Aleph235 <akram239911@gmail.com>
2022-08-18 17:16:31 +0200

    fix

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Sends a POST
-curl -s "$1" -X POST -d "email=hr@holbertonschool.com" -d "subject=I will always be here for PLD"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
