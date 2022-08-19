acebf0435c2adadfbe68e86babd2329cb043aee1
Aleph235 <akram239911@gmail.com>
2022-08-19 15:51:47 +0200

    correcting pycodestyle

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,6 +3,6 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-print("\t- type:",type(html))
-print ("\t- content:",html)
-print ("\t- utf8 content:",html.decode("utf-8"))
+print("\t- type:", type(html))
+print("\t- content:", html)
+print("\t- utf8 content:", html.decode("utf-8"))
