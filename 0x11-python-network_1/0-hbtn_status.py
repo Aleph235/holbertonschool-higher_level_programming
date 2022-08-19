31425b0e7dc1631320759235577c81865012bfae
Aleph235 <akram239911@gmail.com>
2022-08-19 15:55:00 +0200

    add documentation

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""fetches https://intranet.hbtn.io/status"""
 import urllib.request
-""" fetches https://intranet.hbtn.io/status """
+
+
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
